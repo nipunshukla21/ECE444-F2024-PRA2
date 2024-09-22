b56d899a5f5fe1d2d34d05ce2850b2633d2a5895
nipunshukla21 <nipunshukla002@gmail.com>
2024-09-22 16:38:21 -0400

    Completed activity 4 and added u of t email form

--- hello.py
+++ hello.py
@@ -4,13 +4,14 @@ from flask_moment import Moment
 import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your U of T email?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,9 +22,19 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+
+        old_email = session.get('email')
+        if old_email is not None and old_name != form.email.data:
+            flash('Looks like you have changed your email!')
+        email = form.email.data
+        if "@mail.utoronto.ca" in email:
+            session['email'] = email
+        else:
+            session['email'] = None
+
         return redirect(url_for('index'))
     return render_template('index.html',
-        form = form, name = session.get('name'))
+        form = form, name = session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
