597a6d71eed5508bd17022f5e9fb99b4f2bce870
nipunshukla21 <nipunshukla002@gmail.com>
2024-09-22 16:22:53 -0400

    Completed chapter 4 exercises up to 4-7 and implemented form

--- hello.py
+++ hello.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
     return render_template('index.html',
-                           current_time=datetime.datetime.now(datetime.UTC))
+        form = form, name = session.get('name'))
+
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
